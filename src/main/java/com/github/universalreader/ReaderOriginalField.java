99f631836707c4a967be909eed96f33803aedb67
boggard <varyanik2@yandex.ru>
2019-02-19 12:50:27 +0300

    feat: reader utilities for fields setting

---
+++ src/main/java/com/github/universalreader/ReaderOriginalField.java
@@ -0,0 +1,27 @@
+package com.github.universalreader;
+
+import lombok.Getter;
+
+public class ReaderOriginalField {
+
+    public static ReaderOriginalField DEFAULT_NULLABLE = new ReaderOriginalField(null, true);
+    public static ReaderOriginalField DEFAULT_NONNULL = new ReaderOriginalField(null, false);
+
+    @Getter
+    private String value;
+    private boolean valid;
+
+    public ReaderOriginalField(String value, boolean valid) {
+        this.value = value;
+        this.valid = valid;
+    }
+
+    public boolean isInvalid() {
+        return !valid;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}
---
+++ src/main/java/com/github/universalreader/contenthandler/ContentHandlerUtil.java
@@ -0,0 +1,40 @@
+package com.github.universalreader.contenthandler;
+
+import com.github.universalreader.ReaderOriginalField;
+import lombok.experimental.UtilityClass;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+@UtilityClass
+public class ContentHandlerUtil {
+
+    public static <T> void setCalculatedField(String originalValue, Function<String, T> mapper, Consumer<T> validSetter,
+                                               Consumer<ReaderOriginalField> invalidSetter, boolean nullable) {
+        try {
+            if (originalValue != null) {
+                T value = mapper.apply(originalValue);
+                setField(originalValue, value, validSetter, invalidSetter, false);
+            } else {
+                setField(null, null, validSetter, invalidSetter, nullable);
+            }
+        } catch (Exception ex) {
+            setField(originalValue, null, validSetter, invalidSetter, false);
+        }
+    }
+
+    public static void setField(String originalValue, Consumer<String> validSetter,
+                                 Consumer<ReaderOriginalField> invalidSetter, boolean nullable) {
+        setField(originalValue, originalValue, validSetter, invalidSetter, nullable);
+    }
+
+    public static <T> void setField(String originalValue, T value, Consumer<T> validSetter,
+                                     Consumer<ReaderOriginalField> invalidSetter, boolean nullable) {
+        if (nullable || value != null) {
+            validSetter.accept(value);
+            invalidSetter.accept(new ReaderOriginalField(originalValue, true));
+        } else {
+            invalidSetter.accept(new ReaderOriginalField(originalValue, false));
+        }
+    }
+}
