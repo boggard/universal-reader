c90f4adcaa8943c79009a6ff25ba877709434fb3
boggard <varyanik2@yandex.ru>
2019-12-04 12:05:31 +0300

    feat(ContentHandlerUtil): utility methods for set parsed field with validation

--- src/main/java/com/github/boggard/universalreader/contenthandler/ContentHandlerUtil.java
+++ src/main/java/com/github/boggard/universalreader/contenthandler/ContentHandlerUtil.java
@@ -9,28 +9,76 @@ import java.util.function.Function;
 @UtilityClass
 public class ContentHandlerUtil {
 
+    /**
+     * Use {@link this#setCalculatedField(String, Function, Consumer, Consumer)}
+     * or {@link this#setCalculatedField(String, Function, Consumer, Consumer, Function)}
+     * instead
+     */
+    @Deprecated
     public static <T> void setCalculatedField(String originalValue, Function<String, T> mapper, Consumer<T> validSetter,
-                                               Consumer<ReaderOriginalField> invalidSetter, boolean nullable) {
+                                              Consumer<ReaderOriginalField> invalidSetter, boolean nullable) {
+        setCalculatedField(originalValue, mapper, validSetter, invalidSetter, val -> nullable || val != null);
+    }
+
+    public static <T> void setCalculatedField(String originalValue, Function<String, T> mapper, Consumer<T> validSetter,
+                                              Consumer<ReaderOriginalField> invalidSetter) {
+        setCalculatedField(originalValue, mapper, validSetter, invalidSetter, val -> true);
+    }
+
+    public static <T> void setCalculatedField(String originalValue, Function<String, T> mapper, Consumer<T> validSetter,
+                                              Consumer<ReaderOriginalField> invalidSetter, Function<T, Boolean> validator) {
         try {
             if (originalValue != null) {
                 T value = mapper.apply(originalValue);
-                setField(originalValue, value, validSetter, invalidSetter, false);
+                setField(originalValue, value, validSetter, invalidSetter, validator);
             } else {
-                setField(null, null, validSetter, invalidSetter, nullable);
+                setField(null, null, validSetter, invalidSetter, validator);
             }
         } catch (Exception ex) {
-            setField(originalValue, null, validSetter, invalidSetter, false);
+            setField(originalValue, null, validSetter, invalidSetter, validator);
         }
     }
 
+    /**
+     * Use {@link this#setField(String, Consumer, Consumer)}
+     * or {@link this#setField(String, Consumer, Consumer, Function)}
+     * instead
+     */
+    @Deprecated
     public static void setField(String originalValue, Consumer<String> validSetter,
-                                 Consumer<ReaderOriginalField> invalidSetter, boolean nullable) {
-        setField(originalValue, originalValue, validSetter, invalidSetter, nullable);
+                                Consumer<ReaderOriginalField> invalidSetter, boolean nullable) {
+        setField(originalValue, validSetter, invalidSetter, val -> nullable || val != null);
+    }
+
+    public static void setField(String originalValue, Consumer<String> validSetter,
+                                Consumer<ReaderOriginalField> invalidSetter) {
+        setField(originalValue, originalValue, validSetter, invalidSetter, val -> true);
+    }
+
+    public static void setField(String originalValue, Consumer<String> validSetter,
+                                Consumer<ReaderOriginalField> invalidSetter, Function<String, Boolean> validator) {
+        setField(originalValue, originalValue, validSetter, invalidSetter, validator);
+    }
+
+    /**
+     * Use {@link this#setField(String, Object, Consumer, Consumer)}
+     * or {@link this#setField(String, Object, Consumer, Consumer, Function)}
+     * instead
+     */
+    @Deprecated
+    public static <T> void setField(String originalValue, T value, Consumer<T> validSetter,
+                                    Consumer<ReaderOriginalField> invalidSetter, boolean nullable) {
+        setField(originalValue, value, validSetter, invalidSetter, val -> nullable || val != null);
     }
 
     public static <T> void setField(String originalValue, T value, Consumer<T> validSetter,
-                                     Consumer<ReaderOriginalField> invalidSetter, boolean nullable) {
-        if (nullable || value != null) {
+                                    Consumer<ReaderOriginalField> invalidSetter) {
+        setField(originalValue, value, validSetter, invalidSetter, val -> true);
+    }
+
+    public static <T> void setField(String originalValue, T value, Consumer<T> validSetter,
+                                    Consumer<ReaderOriginalField> invalidSetter, Function<T, Boolean> validator) {
+        if (validator.apply(value)) {
             validSetter.accept(value);
             invalidSetter.accept(new ReaderOriginalField(originalValue, true));
         } else {
